5dba4dd5fc487b46300ebad2e92eb8e5c1d2a53a
charbelnohra <nohracharbel113@gmail.com>
2024-08-04 11:48:31 +0300

    changing a bit the format

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let posts = [];
 
